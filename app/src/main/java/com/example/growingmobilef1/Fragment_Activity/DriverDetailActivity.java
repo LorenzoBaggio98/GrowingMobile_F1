1651ba432f4eeea03eb34a37e433458d0a34fda1
piero <petri_sor@yahoo.com>
2019-07-05 10:40:25 +0200

    inf

--- app/src/main/java/com/example/growingmobilef1/Fragment_Activity/DriverDetailActivity.java
+++ app/src/main/java/com/example/growingmobilef1/Fragment_Activity/DriverDetailActivity.java
@@ -1,8 +1,12 @@
 package com.example.growingmobilef1.Fragment_Activity;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.growingmobilef1.Database.ModelRoom.RoomDriver;
 import com.example.growingmobilef1.Model.Driver;
@@ -20,6 +24,7 @@ public class DriverDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_driver_detail);
 
 
+
         Bundle vBundle=getIntent().getExtras();
         if (vBundle !=null){
            mDriver = (RoomDriver) vBundle.getSerializable("SAVE_ID");
@@ -34,9 +39,27 @@ public class DriverDetailActivity extends AppCompatActivity {
             mDriverDetailFragment= (DriverDetailFragment) getSupportFragmentManager().findFragmentByTag(FRAGMENT_DETAGLIO);
         }
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(mDriver.name);
+
 
 
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
 
 
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
 }
